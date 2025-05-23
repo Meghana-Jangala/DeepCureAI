12f02e7f0a3015bb471fda7f58e751c29e971ab5
MeghanaJangala <jangalameghana3306@gmail.com>
2025-05-23 21:17:55 +0530

    updated css

--- client/src/components/3DViewer/ThreeDViewer.jsx
+++ client/src/components/3DViewer/ThreeDViewer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-// import './Visualization.css';
+import './Visualization.css';
 
 const ThreeDViewer = () => {
     const [selectedFile, setSelectedFile] = useState(null);
